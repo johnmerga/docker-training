ee5c4edc5f6323e343a29a993906190602ab190e
johnmerga <johnmerga@yahoo.com>
2022-05-01 07:20:44 +0300

    readme

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 // write basic express server   //
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.json());
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB: ", err.message);
+  });
 app.get("/", (req, res) => {
   res.send("Hello World! from Docker and it works.");
 });
