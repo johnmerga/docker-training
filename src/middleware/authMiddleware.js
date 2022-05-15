7fc809eeadd50c648ae44a2642176cdf2762a321
johnmerga <johnmerga@yahoo.com>
2022-05-15 05:29:02 +0300

    minor update

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,7 +3,7 @@ const authMiddleware = (req, res, next) => {
   if (!req.session.user) {
     return res.status(401).json({ message: "You must be logged in" });
   }
-  
+  req.user = req.session.user;
   next();
 };
 
